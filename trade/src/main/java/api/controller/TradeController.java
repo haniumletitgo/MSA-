a62fab36d3dbd8db75ec73d40c941d8252ea2431
xogml123 <xogml951@gmail.com>
2022-06-02 03:17:39 +0900

    feat: TradeController updateTrade deleteTrade end point 추가 UpdateTradeRequest, UpdateTradeResponse, DeleteTradeResponse Dto 추가. (#8)

---
+++ trade/src/main/java/api/controller/TradeController.java
@@ -0,0 +1,50 @@
+package api.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TradeController {
+
+
+
+    @PutMapping("/trade/{id}")
+    public UpdateTradeResponse updateTrade(@PathVariable("id") Long id, @RequestBody UpdateTradeRequest request) {
+        //tradeService.overwriteTrade(request.toEntity);
+        return new UpdateTradeResponse(id);
+    }
+
+    @DeleteMapping("/trade/{id}")
+    public DeleteTradeResponse deleteTrade(@PathVariable("id") Long id) {
+        //Long deleteId = tradeService.delete(id);
+        return new DeleteTradeResponse(id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class UpdateTradeRequest {
+        Long tradeId;
+        //entity내용과 일치
+    }
+
+    @Data
+    @AllArgsConstructor
+    private class UpdateTradeResponse {
+        Long tradeId;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class DeleteTradeResponse {
+        Long tradeId;
+    }
+
+
+}
